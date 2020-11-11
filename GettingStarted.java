502f0732f80403e22f811fe6b4c76916fe623588
Matt Fricker <matt@fricker.io>
2020-11-11 23:03:31 +0000

    change class name

--- GettingStarted.java
+++ GettingStarted.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import models.*;
 
-class QuickStart {
+class GettingStarted {
     public static void main(String[] args) {
         System.out.println("-----------------------------------------: START");
         
