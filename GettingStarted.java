6596753e019295309fee132dcbf8dc59910f0461
Matt Fricker <matt@fricker.io>
2024-12-06 12:19:20 +0000

    refactor and add list functions

--- GettingStarted.java
+++ GettingStarted.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import models.*;
 
@@ -17,29 +19,45 @@ class GettingStarted {
         // array of fruit
         String[] fruitArray = { "Apple", "Banana", "Orange", "Peach", "Grape", "Kiwi", "Pineapple", "Mango" };
 
-        listRandomFruit(fruitArray, fruitArray.length);
+        listFruit(fruitArray);
+        listFruitInRandomOrder(fruitArray);
         listOddFruit(fruitArray);
 
         System.out.println("-----------------------------------------: END");
     }
 
-    private static void listRandomFruit(String[] fruitArray, int listLength) {
+    private static void listFruit(String[] fruitArray) {
         System.out.println();
-        System.out.println("Random Fruit:");
+        System.out.println("Fruit in original order:");
         System.out.println("-----------------------------------------");
+
+        for (String fruit : fruitArray) {
+            System.out.println(fruit);
+        }
+    }
+
+    private static void listFruitInRandomOrder(String[] fruitArray) {
+        System.out.println();
+        System.out.println("Fruit in random order:");
+        System.out.println("-----------------------------------------");
+
+        List<String> randomStrings = new ArrayList<String>();
 
         // loop while i is less or equal to 10
         int i = 1;
-        while (i <= listLength) {
+        while (i <= fruitArray.length) {
             // get random item of fruit
             Random random = new Random();
-            String x = fruitArray[random.nextInt(fruitArray.length)];
+            String randomItem = fruitArray[random.nextInt(fruitArray.length)];
 
-            // print out fruit
-            System.out.println(i + " " + x);
-            
-            // increment
-            i++;
+            if (!randomStrings.contains(randomItem)) {
+                randomStrings.add(randomItem);
+                i++;
+            }
+        }
+
+        for (String fruit : randomStrings) {
+            System.out.println(fruit);
         }
     }
 
@@ -50,7 +68,7 @@ class GettingStarted {
 
         for (int i = 0; i < fruitArray.length; i++){
             if (i % 2 != 0) {
-                System.out.println("Odd fruit: " + fruitArray[i]);
+                System.out.println(fruitArray[i]);
             }
         }
     }
